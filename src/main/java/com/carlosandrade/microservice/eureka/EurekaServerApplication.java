6ec29e81383f636c74e5fc415d1e6e0495370cd3
Carlos Martins <carlos.martins@engdb.com.br>
2021-02-10 11:29:50 -0300

    readme

--- src/main/java/com/carlosandrade/microservice/eureka/EurekaServerApplication.java
+++ src/main/java/com/carlosandrade/microservice/eureka/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package br.com.alura.microservice.eureka;
+package com.carlosandrade.microservice.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
